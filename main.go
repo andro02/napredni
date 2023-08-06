0415ebf42f628db93911f0b5e952a4c5cd730e89
andro02 <andrija.slovic1@gmail.com>
2023-08-06 16:50:12 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	reader := bufio.NewReader(os.Stdin)
+	var commands = [5]string{"PUT", "GET", "DELETE", "LIST", "RANGESCAN"}
+	var tokens []string
+
+	for {
+		fmt.Print(">>> ")
+		input, err := reader.ReadString('\n')
+		if err != nil {
+			log.Fatal(err)
+		}
+		tokens = strings.Split(input, " ")
+
+		var found bool = false
+		for _, command := range commands {
+			if tokens[0] == command {
+				found = true
+				break
+			}
+		}
+
+		if found {
+			fmt.Println(tokens[0])
+			break
+		} else {
+			fmt.Println("Invalid command. Please try again.")
+		}
+	}
+
+	switch tokens[0] {
+
+	case commands[0]:
+		{
+			fmt.Println("PUT code")
+		}
+	case commands[1]:
+		{
+			fmt.Println("GET code")
+		}
+	case commands[2]:
+		{
+			fmt.Println("DELETE code")
+		}
+	case commands[3]:
+		{
+			fmt.Println("LIST code")
+		}
+	case commands[4]:
+		{
+			fmt.Println("RANGESCAN code")
+		}
+
+	}
+
+}
